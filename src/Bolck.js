eb544b5868e8a8c9c70d3a9bf483a66156c5e32a
zhangrh <zhangrhweb@163.com>
2018-03-20 20:59:03 +0800

    complate initBlock

--- src/Bolck.js
+++ src/Bolck.js
@@ -1,9 +1,13 @@
 // 每一个存在的方格
 
 export default class {
-  constructor(postion, num) {
-    this.postion = postion
-    this.num = num
+  constructor(postion) {
+    // 位置置顶的是在数组中的位置 ['在列的位置', '在行的位置']
+    this.isBlank = true
+    if (postion) {
+      this.postion = postion
+    }
+    this.num = Math.random() > 0.5 ? 1 : 2
   }
 
   move(direction) {
--- src/index.js
+++ src/index.js
@@ -1,28 +1,54 @@
 import './style/style.css'
+// import Bolck from './Bolck'
+
+var Bolck = function (postion) {
+  this.isBlank = true
+  if (postion) {
+    this.postion = postion
+    this.num = Math.random() > 0.5 ? 1 : 2
+  }
+}
+
+Bolck.prototype.move = function (direction) {
+  console.log(direction)
+}
+
 
 
 // 创造地图
-const row = 6
-const col = 9
-var mapArr = new Array
-for(let i = 0 ; i < col; i++) {
-  mapArr.push(new Array(row).fill(new Object))
+var creatMap = function () {
+  const row = 6
+  const col = 9
+  var mapArr = new Array
+  for (let i = 0; i < col; i++) {
+    // mapArr.push(new Array(row).fill(new Object))
+    mapArr.push(new Array(row).fill(new Bolck))
+  }
+  return mapArr
 }
-console.log(mapArr)
+var mapArr = creatMap()
 
 
+// 初始化前三个方块
+var initBolck = function () {
+  for (let i = 0; i < 3; i++) {
+    var block = new Bolck({
+      x: i + 1,
+      y: 0
+    })
+    block.isBlank = false
+    mapArr[block.postion.y][block.postion.x] = block
+  }
+}
+initBolck()
 
 
 
 // 渲染函数
 // 根据地图生成指定DIV
-var html = require('./html/template.art')(mapArr)
-document.getElementById('app').innerHTML = html
-
-
-
-
-
-
-
-
+var render = function (mapArr) {
+  console.log(mapArr)
+  var html = require('./html/template.art')(mapArr)
+  document.getElementById('app').innerHTML = html
+}
+render(mapArr)
